a46c849176f5e955c44037dee3e4df68bdd64ae0
Noirakumaaa <144528285+Noirakumaaa@users.noreply.github.com>
2024-11-26 21:20:51 +0800

    setup next js with redux

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from "next";
+
+
+export default function IndexPage() {
+  return <div>hello world</div>;
+}
+
+export const metadata: Metadata = {
+  title: "TaskTracker",
+};
